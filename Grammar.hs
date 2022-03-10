363fa5e8b4b944968528b82a323b6088b2e0d62d
OndraKrejci <o.krejci13@gmail.com>
2022-03-10 13:48:54 +0100

    Grammar and Input comments

--- Grammar.hs
+++ Grammar.hs
@@ -4,11 +4,13 @@ module Grammar (Rule(..), CFG(..)) where
 import qualified Data.Set as Set
 import qualified Data.List as List
 
+-- Rule of a context-free grammar
 data Rule = Rule {
     _left :: String,
     _right :: [String]
 } deriving (Eq, Ord)
 
+-- Context-free grammar
 data CFG = CFG {
     _nonterms :: Set.Set String,
     _terms :: Set.Set String,
@@ -17,9 +19,11 @@ data CFG = CFG {
 } deriving Eq
 
 instance Show Rule where
+    -- String representation of a rule
     show (Rule left right) = left ++ "->" ++ List.intercalate "" right
 
 instance Show CFG where
+    -- String representation of a CFG
     show (CFG nonterms terms rules start) =
         List.intercalate "," (Set.toList nonterms) ++ "\n" ++
         List.intercalate "," (Set.toList terms) ++ "\n" ++
--- Input.hs
+++ Input.hs
@@ -8,9 +8,12 @@ import qualified Data.List as List
 import Grammar (CFG(CFG), Rule(Rule))
 import Util (splitStr)
 
+-- Reads all input from a file if a file path is supplied, otherwise uses stdin
 readInput :: Maybe String -> IO String
 readInput = maybe IO.getContents IO.readFile
 
+-- Creates a CFG representation from input strings
+-- Expected format: nonterminals, terminals, start symbol, rule (one or more)
 parseGrammar :: [String] -> CFG
 parseGrammar ls
   | length ls < 4 = error "Grammar definition shorter than 4 lines"
@@ -22,6 +25,8 @@ parseGrammar ls
       start = parseStartSymbol (ls!!2) nonterms
     in CFG nonterms terms rules start
 
+-- Creates a set of nonterminal symbols from a comma separated list
+-- Nonterminal must be an upper case letter
 parseNonterms :: String -> Set.Set String
 parseNonterms line =
   let
@@ -33,6 +38,9 @@ parseNonterms line =
         else error ("Invalid nonterminal symbol [" ++ symbol ++ "]")
   in foldr fn Set.empty symbols
 
+-- Creates a set of terminal symbols from a comma separated list
+-- Terminal must be a lower case letter,
+-- also allows some additional symbols not specified in the requirements (used in tests)
 parseTerms :: String -> Set.Set String
 parseTerms line =
   let
@@ -44,16 +52,22 @@ parseTerms line =
         else error ("Invalid terminal symbol [" ++ symbol ++ "]")
   in foldr fn Set.empty symbols
 
+-- Validates that the start symbol is a nonterminal
 parseStartSymbol :: String -> Set.Set String -> String
 parseStartSymbol start nonterms =
   if Set.member start nonterms
     then start
     else error ("Start symbol [" ++ start ++ "] is not a nonterminal symbol")
 
+-- Parses the lines of input into a set of rules
 parseRules :: [String] -> Set.Set String -> Set.Set String -> Set.Set Rule
 parseRules ls nonterms terms =
   foldr (\line acc -> Set.insert (parseRule line nonterms terms) acc) Set.empty ls
 
+-- Parses a string into a rule of a context-free grammar
+-- Expects the rule to be separated with '->' with a nonterminal on the left side
+-- and one or more symbols on the right side (each of them only one character)
+-- Checks that the symbols in the rule belong to the existing sets of symbols
 parseRule :: String -> Set.Set String -> Set.Set String -> Rule
 parseRule line nonterms terms =
   let
