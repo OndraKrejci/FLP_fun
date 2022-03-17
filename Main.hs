1b07a900e5f15619110c3ac1cc2830e54eb6e4a1
OndraKrejci <o.krejci13@gmail.com>
2022-03-17 16:10:23 +0100

    Added data type for mode argument

--- Main.hs
+++ Main.hs
@@ -4,48 +4,50 @@ import qualified System.Environment as Environment
 import Algorithm (removeSimpleRules, createCNF)
 import Input (readInput, parseGrammar)
 
+data Mode = Print | RemoveSimple | Chomsky | Help
+  deriving (Eq, Show)
+
 -- Main function, loads the CFG from input a prints it in the requested form
 main :: IO ()
 main = do
   args <- Environment.getArgs
   let (mode, fpath) = parseArgs args
-  if mode == "h"
+  if mode == Help
     then do
       putStr help
-      return ()
     else do
       contents <- readInput fpath
       let grammar = parseGrammar (lines contents)
       case mode of
-        "i" -> print grammar
-        "1" -> print (removeSimpleRules grammar)
-        "2" -> print (createCNF (removeSimpleRules grammar))
-        _ -> error ("Invalid mode [" ++ mode ++ "]")
-      return ()
+        Print -> print grammar
+        RemoveSimple -> print (removeSimpleRules grammar)
+        Chomsky -> print (createCNF (removeSimpleRules grammar))
+        _ -> error ("Invalid mode [" ++ show mode ++ "]")
 
 -- Parses the given list of arguments into a mode and a file path
--- Mode is required, file path is optional, more arguments are not allowed
--- Additionally returns the "h" mode if no arguments are supplied
-parseArgs :: [String] -> (String, Maybe String)
-parseArgs [] = ("h", Nothing)
+-- Mode is required, file path is optional, more than two arguments are not allowed
+-- Additionally returns the Help mode for an empty list of arguments
+parseArgs :: [String] -> (Mode, Maybe String)
+parseArgs [] = (Help, Nothing)
 parseArgs [mode] = (parseMode mode, Nothing)
 parseArgs [mode, path] = (parseMode mode, Just path)
-parseArgs _ = error ("Error: Supplied more than two arguments\n" ++ help)
+parseArgs _ = error ("Supplied more than two arguments\n" ++ help)
 
--- Checks if the mode is valid and removes the leading '-'
-parseMode :: String -> String
+-- Parses the mode from an argument
+parseMode :: String -> Mode
 parseMode str = case str of
-  "-i" -> "i"
-  "-1" -> "1"
-  "-2" -> "2"
-  _ -> error ("Error: Invalid argument [" ++ str ++ "]\n" ++ help)
+  "-i" -> Print
+  "-1" -> RemoveSimple
+  "-2" -> Chomsky
+  _ -> error ("Invalid argument [" ++ str ++ "]\n" ++ help)
 
--- Help test for the program
+-- Help text for the program
 help :: String
 help =
+  "Transform a context-free grammar without simple rules into Chomsky normal form\n" ++
   "Usage:\n" ++
   "./flp-fun <mode> [path]\n\n" ++
   "mode:\n" ++
   " -i: print loaded grammar\n" ++
   " -1: print grammar without simple rules\n" ++
-  " -2: print grammar without cnf\n"
+  " -2: print grammar in CNF\n"
